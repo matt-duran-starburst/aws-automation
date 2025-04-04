2ac5c41c03a3fc9b439817dd6fa83279854468a4
“karina_and” <karina.etova@gmail.com>
2025-04-04 16:42:30 -0700

    add authorization to test job

--- main.tf
+++ main.tf
@@ -5,6 +5,12 @@ terraform {
       version = "4.25.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name  = var.resource_group_name
+    storage_account_name = var.storage_account_name
+    container_name       = var.container_name
+    key                  = var.key
+  }
 }
 
 provider "azurerm" {
